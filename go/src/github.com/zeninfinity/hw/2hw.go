680b629ac7623d42a5c1791fe47ad9b16015c03b
Zac Zager <zzager@yieldex.com>
2014-09-29 14:51:10 -0600

    ZZ - new go tutorial stuff

---
+++ go/src/github.com/zeninfinity/hw/2hw.go
@@ -0,0 +1,10 @@
+package main
+import (
+  "fmt"
+  "github.com/zeninfinity/newmath"
+)
+
+func main(){
+  fmt.Println("Hello you amazing world, you sexy mother fucker, you...")
+  fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
